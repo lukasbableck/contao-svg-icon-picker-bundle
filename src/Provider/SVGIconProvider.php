7e2cbf3c1f2b66ea572b812617801f11999e939e
Lukas Bableck <lukas@bableck.dev>
2025-08-19 14:01:25 +0200

    improve performance

--- src/Provider/SVGIconProvider.php
+++ src/Provider/SVGIconProvider.php
@@ -2,35 +2,31 @@
 namespace Lukasbableck\ContaoSVGIconPickerBundle\Provider;
 
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\Yaml\Yaml;
 
 class SVGIconProvider {
 	private array $cache = [];
 
 	public function __construct(
 		#[Autowire('%kernel.project_dir%')]
-		private readonly string $projectDir
+		private readonly string $projectDir,
 	) {
 	}
 
 	public function getIcons(string $sourceDirectory, ?string $metadataDirectory): array {
 		$sourceDirectory = rtrim($this->projectDir, '/').'/'.ltrim($sourceDirectory, '/');
+		if (isset($this->cache[$sourceDirectory])) {
+			return $this->cache[$sourceDirectory];
+		}
+		$this->buildIconCache($sourceDirectory, $metadataDirectory);
+
+		return $this->cache[$sourceDirectory] ?? [];
+	}
+
+	private function buildIconCache(string $sourceDirectory, ?string $metadataDirectory): void {
 		if (!is_dir($sourceDirectory)) {
 			throw new \RuntimeException(\sprintf('The source directory "%s" does not exist.', $sourceDirectory));
 		}
 
-		if (isset($this->cache[$sourceDirectory])) {
-			return $this->cache[$sourceDirectory];
-		}
-
-		if (!isset($this->cache[$sourceDirectory])) {
-			$this->buildIconCache($sourceDirectory, $metadataDirectory);
-		}
-
-		return $this->cache[$sourceDirectory];
-	}
-
-	private function buildIconCache(string $sourceDirectory, ?string $metadataDirectory): void {
 		$metadata = $this->getMetadata($metadataDirectory);
 		$icons = [];
 		foreach (glob($sourceDirectory.'/*.svg') as $file) {
@@ -61,26 +57,16 @@ class SVGIconProvider {
 			throw new \RuntimeException(\sprintf('The metadata directory "%s" does not exist.', $metadataDirectory));
 		}
 
-		$categories = [];
-		$categoriesFile = $metadataDirectory.'/categories.yml';
-		if (file_exists($categoriesFile)) {
-			$categories = Yaml::parseFile($categoriesFile);
-			if (false === $categories) {
-				throw new \RuntimeException(\sprintf('Failed to parse YAML file: %s', $categoriesFile));
-			}
-		}
-
 		$icons = [];
-		$iconsFile = $metadataDirectory.'/icons.yml';
+		$iconsFile = $metadataDirectory.'/icons.json';
 		if (file_exists($iconsFile)) {
-			$icons = Yaml::parseFile($iconsFile);
+			$icons = json_decode(file_get_contents($iconsFile), true);
 			if (false === $icons) {
-				throw new \RuntimeException(\sprintf('Failed to parse YAML file: %s', $iconsFile));
+				throw new \RuntimeException(\sprintf('Failed to parse JSON file: %s', $iconsFile));
 			}
 		}
 
 		return [
-			'categories' => $categories,
 			'icons' => $icons,
 		];
 	}
