ea5e025459610249799d99c2ff9ced9b188bbd1f
Lukas Bableck <lukas@bableck.dev>
2026-01-15 10:54:50 +0100

    Add close button to picker

--- contao/languages/de/svg_icon_picker.php
+++ contao/languages/de/svg_icon_picker.php
@@ -1,4 +1,5 @@
 <?php
 
+$GLOBALS['TL_LANG']['close'] = 'Icon-Auswahl schließen';
 $GLOBALS['TL_LANG']['search'] = 'Suchen';
 $GLOBALS['TL_LANG']['edit'] = 'Icon auswählen';
--- contao/languages/en/svg_icon_picker.php
+++ contao/languages/en/svg_icon_picker.php
@@ -1,4 +1,5 @@
 <?php
 
+$GLOBALS['TL_LANG']['close'] = 'Close icon picker';
 $GLOBALS['TL_LANG']['search'] = 'Search';
 $GLOBALS['TL_LANG']['edit'] = 'Select icon';
